75ca224c543472cfca4b30a5dbbfbfe687c901c3
Martin Erren <martin3r@Mac.fritz.box>
2025-09-10 19:33:01 +0200

    allergens & CO

--- resources/views/livewire/brand/index.blade.php
+++ resources/views/livewire/brand/index.blade.php
@@ -1,3 +1,6 @@
 <div>
-    <h1>Hallo Welt</h1>
+    <div class="d-flex justify-between items-center mb-4">
+        <h1 class="text-xl font-semibold">Brands</h1>
+        <x-ui-button variant="primary" wire:click="openCreateModal">New Brand</x-ui-button>
+    </div>
 </div>
